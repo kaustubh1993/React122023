a1aeedc395184681f62dca0906f4e73d90f8a320
kaustubh1993 <kaustubhwarokar@gmail.com>
2023-07-04 21:15:25 +0530

    React Fragment and React Component and React Element

--- App.js
+++ App.js
@@ -1,24 +1,59 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 
-const parent = React.createElement("div", { id: "parent" }, [
-React.createElement("div", { id: "child" },[ 
-React.createElement("h1", {}, "I am h1 tag"),
-React.createElement("h2", {}, "I am h2 tag")
-]),
-React.createElement("div", { id: "child2" },[ 
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag")
-    ])
-])
+
+//React Element 
+const title =  (
+    <h1 className='head' id="heading">Namaste From React 😊😂</h1>
+    );
+
+const Title = () => (
+<h1 className='head' id="heading">Namaste From React 😊</h1>
+);
+
+// const HeaderComponent = () =>{
+//     return <h1>Namaste React Functional Component 🚀</h1>
+// }
+//React Fragment  - behave like empty tag
+const HeaderComponent = () =>( 
+
+    <React.Fragment>
+     <div id="container">
+      {/* {title} */}
+      <Title/>
+     <h1>Namaste React Functional Component 🚀</h1>
+     <div id='container-2'></div>
+     </div>
+     </React.Fragment>
+
+     );
+
+// const HeaderComponent1 = () => <h1>Namaste React Functional Component 🚀</h1>
 
 
-const heading = React.createElement(
-    "h1",
-    { id: "heading", xyz: "abc" },
-    "Hello world from React");
-
-//heading is just object created using React.createElement 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);  // this method will put heading object to dom
-root.render(parent);  
+// root.render(jsxHeading);
+root.render(<HeaderComponent/>)
+
+
+// const parent = React.createElement("div", { id: "parent" }, [
+// React.createElement("div", { id: "child" },[ 
+// React.createElement("h1", {}, "I am h1 tag"),
+// React.createElement("h2", {}, "I am h2 tag")
+// ]),
+// React.createElement("div", { id: "child2" },[ 
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("h2", {}, "I am h2 tag")
+//     ])
+// ])
+
+
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading", xyz: "abc" },
+//     "Hello world from React");
+
+// //heading is just object created using React.createElement 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(heading);  // this method will put heading object to dom
+// root.render(parent);  
